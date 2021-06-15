57c3cd8f27e89ba77d4158b524e311ad8734b7ce
scor4er <scor4er@gmail.com>
2021-06-15 14:13:11 +0300

    Sample updated to use multiple projects. Client projects only need Microsoft.Extensions.Logging.Abstractions reference

---
+++ LoggingSample/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using LoggingSample.Blah;
+using LoggingSample.Messaging;
+using Messaging;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Filters;
+using Unity;
+using Unity.Microsoft.Logging;
+
+namespace LoggingSample
+{
+    internal class Program
+    {
+        private static readonly IUnityContainer Container = new UnityContainer();
+
+        private static void Main(string[] args)
+        {
+            Container.RegisterType<IBlahService, BlahService>();
+            Container.RegisterType<ISimpleMessageLogService, SimpleMessageLogService>();
+            RegisterLogging();
+
+            var blahService = Container.Resolve<IBlahService>();
+            blahService.Blah();
+
+            var simpleMessageService = Container.Resolve<ISimpleMessageLogService>();
+            simpleMessageService.Message();
+
+            Console.ReadKey();
+        }
+
+        private static void RegisterLogging()
+        {
+            var loggerFactory = new LoggerFactory();
+            var loggerConfig = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Logger(l => l.Filter.ByIncludingOnly(Matching.FromSource("LoggingSample.Blah"))
+                    .WriteTo.Console(
+                        outputTemplate:
+                        "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] [{SourceContext}] {Message}{NewLine}{Exception}"))
+                .WriteTo.Logger(l => l.Filter.ByIncludingOnly(Matching.FromSource("LoggingSample.Messaging"))
+                    .WriteTo.Console(outputTemplate: "{Message}"))
+                .CreateLogger();
+
+            loggerFactory.AddSerilog(loggerConfig);
+
+            Container.AddExtension(new LoggingExtension(loggerFactory));
+        }
+    }
+}
